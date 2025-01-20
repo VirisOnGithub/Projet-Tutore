044456b351dd32f66e384dc3abfa8d5eb9147eb9
Clément <clement.reniers@etu.univ-lyon1.fr>
2025-01-20 08:58:51 +0100

    forgot Badge.vue

---
+++ components/Badge.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center bg-gray-500 px-2 rounded">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+/* Scoped styles removed since TailwindCSS is used */
+</style>
