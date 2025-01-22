0f4310d600a87cd2ea749e693ad58f9b6108e8a0
Luquin Emmanuel p2202699 <emmanuel.luquin@etu.univ-lyon1.fr>
2025-01-22 17:18:32 +0100

    placed carousel in center

--- pages/browse.vue
+++ pages/browse.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="flex h-screen w-screen justify-center items-center bg-[#202020]">
-    <img src="/index_backdrop.webp" alt="Background Image" class="fixed top-0 left-0 w-full h-full object-cover z-[-2]">
-    <div id="gray-filter" class="absolute inset-0 z-[-1]" style="background: rgba(0, 0, 0, 0.8);"></div>
+  <div class="flex absolute top-0 left-0 h-screen w-screen justify-center items-center bg-[#202020]">
     <MovieCarousel />
   </div>
 </template>
