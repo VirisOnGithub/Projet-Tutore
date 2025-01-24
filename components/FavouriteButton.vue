cda198df7254a0f8b767e40be6a61a909ab43887
Clément <clement.reniers@etu.univ-lyon1.fr>
2025-01-24 17:08:06 +0100

    `fix` : style for buttons

--- components/FavouriteButton.vue
+++ components/FavouriteButton.vue
@@ -45,31 +45,31 @@ onMounted(async () => {
   <button
       @click="addToFavouriteList"
       class="rounded-lg relative w-56 h-8 cursor-pointer flex items-center border border-black-500 bg-black-500 group hover:bg-black-600 active:bg-black-700 text-gray-100 hover:text-[#202020] hover:bg-gray-100"
-
   >
-              <span
-                  class="text-inherit font-semibold ml-4 transition-all duration-300"
-              >Add to favourite list</span
-              >
+    <span
+        class="text-inherit font-semibold ml-4 transition-all duration-300"
+    >
+      Add to favourite list
+    </span>
     <span
         class="absolute right-0 h-full w-10 rounded-lg bg-black-500 flex items-center justify-center transition-all duration-300 group-hover:bg-black-600 group-active:bg-black-700"
     >
-                <svg
-                    class="svg w-8 text-inherit"
-                    fill="none"
-                    height="24"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                  <line x1="12" x2="12" y1="5" y2="19"></line>
-                  <line x1="5" x2="19" y1="12" y2="12"></line>
-                </svg>
-              </span>
+      <svg
+          class="svg w-8 text-inherit"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <line x1="12" x2="12" y1="5" y2="19"></line>
+        <line x1="5" x2="19" y1="12" y2="12"></line>
+      </svg>
+    </span>
   </button>
 </template>
 
