3cbe06e8c9281f57628cdc7a25bc67d3bd21523f
Luka COUTANT <luka.coutant@etu.univ-lyon1.fr>
2025-01-29 22:40:36 +0100

    fix : remove unnecessary console logs in login API

--- server/api/login.ts
+++ server/api/login.ts
@@ -38,15 +38,12 @@ export default defineEventHandler(async (event) => {
     const user: User = await luka(username);
 
     if (username === user.user_name && (await verifyPassword(user.user_password, password))) {
-        console.log(await getUserSession(event));
         await setUserSession(event, {
             user: {
                 id: user.id_user,
                 username: user.user_name
             }
         });
-        console.log((await getUserSession(event)));
-        console.log(event.context.session);
         return {}
     }
     throw createError({ statusCode: 401, message: 'Invalid credentials' });
